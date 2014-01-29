Changed averager and best individual scripts to use file open dialog, fixed some bugs.

ALPS/scripts/averager.py
@@ -29,7 +29,8 @@ else:
 	
 fitnesses = np.genfromtxt(filename,delimiter = ',',dtype=float)
 print np.nanmin(fitnesses)
-
+globalMinFitness = np.nanmin(fitnesses)
+globalMinRun = 0
 for i in range(1,numruns):
 	if isMean:
 		filename = rundir +"/" + runname + "/" + "r" + str(i) + "/" + runname + "_meanfit.txt"
@@ -37,22 +38,33 @@ for i in range(1,numruns):
 		filename = rundir +"/"+ runname + "/" + "r" + str(i) + "/" + runname + "_bestfit.txt"
 		
 	new_fitnesses = np.genfromtxt(filename,delimiter = ',',dtype=float) 
-	print np.nanmin(new_fitnesses)
+	minfitness = np.nanmin(new_fitnesses)
+	if minfitness < globalMinFitness:
+		globalMinFitness = minfitness
+		globalMinRun = i
 	fitnesses += new_fitnesses
+	print minfitness
 	
-	
+print "The best run is " + str(globalMinRun)
 fitnesses = fitnesses/numruns
 
 if isMean:
 	np.savetxt("meanavgES.txt", fitnesses, delimiter = ",")
 else:
 	np.savetxt("bestavgES.txt", fitnesses, delimiter = ",")
-print fitnesses
+#print fitnesses
 pyplot.figure()
-pyplot.plot(fitnesses.T)
+totalPlot = pyplot.plot(fitnesses.T)
 pyplot.xlim(0,len(fitnesses[0]))
 pyplot.xlabel("Generation")
 pyplot.ylabel("Fitness")
+numberPlots = len(totalPlot)
+legends = []
+for i in range (0, numberPlots):
+	 legends.append('Level ' + str(i +1))
+	
+if len(legends) > 1:
+	pyplot.legend(totalPlot, legends, prop={'size':12})
 
 if isMean:
 	pyplot.title("Mean Fitness vs. Gen")

ALPS/scripts/plotter_bestindiv.py
@@ -3,23 +3,28 @@ import matplotlib
 # matplotlib.use("Agg")
 from matplotlib import pyplot as plt
 from matplotlib import animation
+import Tkinter, tkFileDialog 
 import sys
 
 u = 20
 
 if len(sys.argv) == 1:
-    print "You can also give filename as a command line argument"
-    filename = raw_input("Enter Filename: ")
-    fps = 5
+    print "You can also give filename as a command line argument."
+    print "Double-click on the file you want to analyze"
+    root = Tkinter.Tk()
+    root.withdraw()
+    full_path = tkFileDialog.askopenfilename(initialdir="../")
+    filename = full_path
+    #runname = raw_input("Enter Run Name: ")
 else:
     filename = sys.argv[1]
 
-indivs = np.genfromtxt(filename, dtype=float, comments='#', delimiter=', ')
+indivs = np.loadtxt(filename, dtype=float, comments='#', delimiter=', ', converters=None, skiprows=0, usecols=None, unpack=False, ndmin=0)
 print indivs
 arity_x2 = len(indivs[0])
 
-imag_values = indivs[:,range(0,arity_x2,2)]
-real_values = indivs[:,range(1,arity_x2,2)]
+imag_values = indivs[:,range(1,arity_x2,2)]
+real_values = indivs[:,range(0,arity_x2,2)]
 print imag_values
 
 # Normalize the points such that the average norm is 1. This is done in GNURadio as well, so it is a realistic depiction. 
@@ -31,14 +36,19 @@ for i in range(0,len(imag_values)):
 	normfactor = normfactor/(arity_x2/2)
 	imag_values[i] = imag_values[i]/normfactor
 	real_values[i] = real_values[i]/normfactor
-
+	
 # First set up the figure, the axis, and the plot element we want to animate
 fig = plt.figure()
-ax = plt.axes(xlim=(-2.5, 2.5), ylim=(-2.5, 2.5))
+
+if len(imag_values[0]) >= 8:
+	ax = plt.axes(xlim=(-2.5, 2.5), ylim=(-2.5, 2.5))
+else:
+	ax = plt.axes(xlim=(-1.5, 1.5), ylim=(-1.5, 1.5))
 points, = ax.plot([], [], 'bo')
 bestpoints, = ax.plot([], [], 'bo')
 
 #ax.figure()
+
 i2 = len(indivs[:,0]) - u -1
 print i2-u
 xb = real_values[i2 ,:]

Island/scripts/averager.py
@@ -29,6 +29,10 @@ else:
 fitnesses = np.genfromtxt(filename,delimiter = ',',dtype=float)
 fitnesses = fitnesses[0:(len(fitnesses)-1)]
 print np.nanmin(fitnesses)
+print np.nanmin(fitnesses)
+globalMinFitness = np.nanmin(fitnesses)
+globalMinRun = 0
+
 for i in range(1,numruns):
 	if isMean:
 		filename = runname + "/" + "r" + str(i) + "/1" + "_mean.txt"
@@ -37,6 +41,11 @@ for i in range(1,numruns):
 		
 	fitnesses2 = np.genfromtxt(filename,delimiter = ',',dtype=float)
 	fitnesses2 = fitnesses2[0:(len(fitnesses2)-1)]
+	
+	minfitness = np.nanmin(fitnesses2)
+	if minfitness < globalMinFitness:
+		globalMinFitness = minfitness
+		globalMinRun = i
 	print np.nanmin(fitnesses2)
 	fitnesses += fitnesses2
 	
@@ -45,16 +54,26 @@ print len(fitnesses)
 print fitnesses
 fitnesses = np.reshape(fitnesses, (len(fitnesses)/numislands, numislands))
 fitnesses = fitnesses.T
-
+print "The best run is " + str(globalMinRun)
 if isMean:
 	np.savetxt("meanavgisland.txt", fitnesses, delimiter = ",")
 else:
 	np.savetxt("bestavgisland.txt", fitnesses, delimiter = ",")
 pyplot.figure()
-pyplot.plot(fitnesses.T)
+totalPlot = pyplot.plot(fitnesses.T)
 pyplot.xlim(0,len(fitnesses[0]))
 pyplot.xlabel("Generation")
 pyplot.ylabel("Fitness")
+
+numberPlots = len(totalPlot)
+
+legends = []
+for i in range (0, numberPlots):
+	 legends.append('Island ' + str(i +1))
+	
+if len(legends) > 1:
+	pyplot.legend(totalPlot, legends, prop={'size':12})
+
 
 if isMean:
 	pyplot.title("Mean Fitness vs. Gen")

Island/scripts/plotter_bestindivs.py
@@ -3,14 +3,19 @@ import matplotlib
 # matplotlib.use("Agg")
 from matplotlib import pyplot as plt
 from matplotlib import animation
+import Tkinter, tkFileDialog 
 import sys
 
 u = 20
 
 if len(sys.argv) == 1:
-    print "You can also give filename as a command line argument"
-    filename = raw_input("Enter Filename: ")
-    fps = 5
+    print "You can also give filename as a command line argument."
+    print "Double-click on the file you want to analyze"
+    root = Tkinter.Tk()
+    root.withdraw()
+    full_path = tkFileDialog.askopenfilename(initialdir="../")
+    filename = full_path
+    #runname = raw_input("Enter Run Name: ")
 else:
     filename = sys.argv[1]
 
@@ -18,8 +23,8 @@ indivs = np.genfromtxt(filename, dtype=float, comments='#', delimiter=', ')
 print indivs
 arity_x2 = len(indivs[0])
 
-imag_values = indivs[:,range(0,arity_x2,2)]
-real_values = indivs[:,range(1,arity_x2,2)]
+imag_values = indivs[:,range(1,arity_x2,2)]
+real_values = indivs[:,range(0,arity_x2,2)]
 print imag_values
 
 # Normalize the points such that the average norm is 1. This is done in GNURadio as well, so it is a realistic depiction. 
@@ -34,15 +39,18 @@ for i in range(0,len(imag_values)):
 
 # First set up the figure, the axis, and the plot element we want to animate
 fig = plt.figure()
-ax = plt.axes(xlim=(-2.5, 2.5), ylim=(-2.5, 2.5))
+if len(imag_values[1]) >= 8:
+	ax = plt.axes(xlim=(-2.5, 2.5), ylim=(-2.5, 2.5))
+else:
+	ax = plt.axes(xlim=(-1.5, 1.5), ylim=(-1.5, 1.5))
 points, = ax.plot([], [], 'bo')
 bestpoints, = ax.plot([], [], 'bo')
 
 #ax.figure()
 
 i2 = len(indivs[:,0])/u
-xb = real_values[u*i2,:]
-yb = imag_values[u*i2,:]
+xb = real_values[u*i2-1,:]
+yb = imag_values[u*i2-1,:]
 
 ax.plot(xb,yb, 'bo')
 